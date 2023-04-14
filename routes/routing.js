915269d5f764f2dc76f9df206c92116d37f03ad8
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-04-14 08:31:28 +0530

    added ping support

--- routes/routing.js
+++ routes/routing.js
@@ -5,9 +5,9 @@ const routes = express.Router();
 
 routes.post("/login", async (req, res, next) => {
   try {
-    let {cipherToken,jwtRefreshToken} = await service.LoginService(req.body);
+    let { cipherToken, jwtRefreshToken } = await service.LoginService(req.body);
     res.status(200);
-    res.json({ cipherToken,jwtRefreshToken });
+    res.json({ cipherToken, jwtRefreshToken });
   } catch (error) {
     next(error);
   }
@@ -26,22 +26,30 @@ routes.post("/register", async (req, res, next) => {
 routes.post("/verifyotp", authObj.authOtp, async (req, res, next) => {
   try {
     if (req.status) {
-      const {accessToken,jwtRefreshToken} = await service.VerifyOtp(req.userid);
-      return res.json({accessToken,jwtRefreshToken}).status(200);
+      const { accessToken, jwtRefreshToken } = await service.VerifyOtp(
+        req.userid
+      );
+      return res.json({ accessToken, jwtRefreshToken }).status(200);
     } else {
-      return res.json({sessionId:false}).status(400);
+      return res.json({ sessionId: false }).status(400);
     }
   } catch (error) {
     next(error);
   }
 });
 routes.post("/verifyaccess", authObj.auth, async (req, res) => {
-    if (req.access) {
-      const accessToken=await service.verifyAccess(req.accessToken);
-       res.json({accessToken:accessToken,userid:req.userid}).status(200);
-    } else {
-      res.json({accessToken:false}).status(400);
-    }
+  if (req.access) {
+    const accessToken = await service.verifyAccess(req.accessToken);
+    res.json({ accessToken: accessToken, userid: req.userid }).status(200);
+  } else {
+    res.json({ accessToken: false }).status(400);
+  }
 });
-
+routes.get("/", async (req, res, next) => {
+  try {
+    res.json("Ping Successful").status(200);
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = routes;
